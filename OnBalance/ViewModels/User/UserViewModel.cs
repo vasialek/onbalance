70bdc0ccb270a9c5dcdd55998d8e79e9e8d88593
Vasinov Aleksej <proglamer@gmail.com>
2012-12-02 20:16:30 +0200

    Dirty version with loading size/quantity from DB

---
+++ OnBalance/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace OnBalance.ViewModels.User
+{
+    public class UserViewModel
+    {
+
+        /// <summary>
+        /// Gets/sets username
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Display(Name = "Is approved")]
+        public bool IsApproved { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Comment { get; set; }
+    }
+}
