6bb26b3834f83d7fc00c934e1564bbe3c28458d6
Aleksej Vasinov <proglamer@gmail.com>
2013-03-19 18:49:33 +0200

    Stub for product parameters administration

---
+++ OnBalance/Controllers/ParamController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnBalance.Models;
+
+namespace OnBalance.Controllers
+{
+
+    public class ParamController : BaseController
+    {
+
+        //
+        // GET: /param/
+
+        public ActionResult Index()
+        {
+            return View(new ParamRepository().Items);
+        }
+
+        //
+        // GET: /param/edit/1
+
+        public ActionResult Edit(int id)
+        {
+            var model = new ParamRepository().Items.Single(x => x.Id == id);
+
+            return View(model);
+        }
+
+    }
+}
---
+++ OnBalance/Models/ParamRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnBalance.Models
+{
+    public class ParamRepository : BaseRepository
+    {
+
+        public IList<ParamModel> Items
+        {
+            get
+            {
+                return new List<ParamModel>
+                {
+                    new ParamModel{ Id = 1001, Name = "Size" },
+                    new ParamModel{ Id = 1002, Name = "Colour" },
+                };
+            }
+        }
+
+    }
+}
