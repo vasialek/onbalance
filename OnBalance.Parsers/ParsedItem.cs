6d27611cf1f95737c9e0964ead9e276a0c916f06
= <proglamer@gmail.com>
2014-06-27 00:01:50 +0300

    Parsing of simple product quantity CSV string with test for GJ Excel

--- OnBalance.Parsers/ParsedItem.cs
+++ OnBalance.Parsers/ParsedItem.cs
@@ -34,5 +34,33 @@ namespace OnBalance.Parsers
 
         public decimal PriceOfRelease { get; set; }
 
+        public IList<ItemSizeQuantity> Sizes { get; set; }
+
+        public ParsedItem()
+        {
+            Sizes = new List<ItemSizeQuantity>();
+        }
+
+        /// <summary>
+        /// Adds 1 product to specific size.
+        /// </summary>
+        public void AddSize(string sizeName)
+        {
+            var size = Sizes.FirstOrDefault(x => x.Size.Equals(sizeName, StringComparison.InvariantCultureIgnoreCase));
+            if(size == null)
+            {
+                // Create new size with one product
+                Sizes.Add(new ItemSizeQuantity { Size = sizeName, Quantity = 1 });
+            } else
+            {
+                // Increase quantity of existing size
+                size.Quantity = size.Quantity + 1;
+            }
+        }
+
+        public int CalculateTotalQuantity()
+        {
+            return Sizes.Sum(x => x.Quantity);
+        }
     }
 }
--- OnBalance.Parsers/Parsers/GjExcelParser.cs
+++ OnBalance.Parsers/Parsers/GjExcelParser.cs
@@ -7,6 +7,9 @@ namespace OnBalance.Parsers.Parsers
 {
     public class GjExcelParser : IBalanceParser
     {
+        protected const int QNT_FIELD = 2;
+        protected const int PRICE_FIELD = 3;
+
         public IList<ParsedItem> ParseFileContent(string[] lines)
         {
             throw new NotImplementedException();
@@ -14,7 +17,107 @@ namespace OnBalance.Parsers.Parsers
 
         public ParsedItem ParseLine(string s)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ParsedItem pi = new ParsedItem();
+                string[] ar = s.Split(new char[] { '\t' });
+                string sizeName;
+
+                for(int i = 0; i < ar.Length; i++)
+                {
+                    if(IsCodeAndProductNameField(i))
+                    {
+                        ParseCodeAndProductName(pi, ar[i].Trim());
+                    } else if(IsQuantityField(i))
+                    {
+                        pi.Quantity = ParseInt(ar[i], "Quantity");
+                    }else if(IsPriceField(i))
+                    {
+                        pi.Price = ParseDecimal(ar[i], "Price");
+                    }else if(IsPriceOfReleaseField(i))
+                    {
+                        pi.PriceOfRelease = ParseDecimal(ar[i].Trim(), "Price of release");
+                    } else
+                    {
+                        sizeName = ar[i].Trim();
+                        if(string.IsNullOrEmpty(sizeName) == false)
+                        {
+                            Console.WriteLine("Index: {0} = {1}", i, ar[i]);
+                            pi.AddSize(sizeName); 
+                        }
+                    }
+                }
+
+                return pi;
+            } catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private bool IsCodeAndProductNameField(int i)
+        {
+            return i == 0;
+        }
+
+        private bool IsQuantityField(int i)
+        {
+            return i == 2;
+        }
+
+        private bool IsPriceField(int i)
+        {
+            return i == 3;
+        }
+
+        private bool IsPriceOfReleaseField(int i)
+        {
+            return i == 25;
+        }
+
+        private void ParseCodeAndProductName(ParsedItem pi, string s)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentNullException("Product code/name field is empty");
+            }
+
+            string[] ar = s.Split(new char[] { ' ' });
+            if(ar == null)
+            {
+                throw new ArgumentException("No separator (space) between product name and code: " + s);
+            }
+
+            pi.ProductName = ar[0].Trim();
+            pi.InternalCode = string.Join(" ", ar, 1, ar.Length - 1);
+        }
+
+        private int ParseInt(string s, string fieldName)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentNullException("`" + fieldName + "` field is empty");
+            }
+            int v;
+            if(int.TryParse(s, out v) == false)
+            {
+                throw new InvalidCastException("Could not parse `" + fieldName + "` from: " + s);
+            }
+            return v;
+        }
+
+        private decimal ParseDecimal(string s, string fieldName)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentNullException("`" + fieldName + "` field is empty");
+            }
+            decimal v;
+            if(decimal.TryParse(s, out v) == false)
+            {
+                throw new InvalidCastException("Could not parse `" + fieldName + "` from: " + s);
+            }
+            return v;
         }
     }
 }
